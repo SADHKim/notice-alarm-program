e7936115061fcd216be0151283f96bfd45e91d25
DHki <kdh101800@gmail.com>
2023-05-12 23:06:04 +0900

    fix: path of pickle file

--- crawling/checking_websites.py
+++ crawling/checking_websites.py
@@ -81,12 +81,12 @@ def return_driver():
     
     
 def get_prev_list():
-    with open('/home/notice-alarm-program/prev.pickle', "rb") as rf:
+    with open('/home/notice-alarm-program/crawling/prev.pickle', "rb") as rf:
         global prev_lists
         prev_lists = pickle.load(rf)
 
 def store_prev_list():
-    with open('/home/notice-alarm-program/prev.pickle', 'wb') as wf:
+    with open('/home/notice-alarm-program/crawling/prev.pickle', 'wb') as wf:
         global prev_lists
         pickle.dump(prev_lists, wf)
         prev_lists = None
